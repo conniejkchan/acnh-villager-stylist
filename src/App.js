048ca4ff70a9a1f5cc0b44d5fe11ebca03619aee
Connie Chan <conniechan@connies-mbp-4.lan>
2020-09-11 18:49:30 -0400

    material design styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
 
 import villagersData from './data/villagers.json';
 import ItemsData from "./data/items.json";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
 
 const App = ({}) => {
 
@@ -18,10 +43,18 @@ const App = ({}) => {
     villagerPhotoList[i] = villagersData[i].photoImage
   }
 
-
+  const classes = useStyles();
   return (
     <>
-    <h1>Animal Crossing New Horizon Villager Stylist</h1>
+    <h1>Animal Crossing New Horizon Villager Stylists</h1>
+    <InputBase
+        className={classes.input}
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+    <IconButton type="submit" className={classes.iconButton} aria-label="search">
+      <SearchIcon />
+    </IconButton>
     {villagersData.map(villager => {
         return (
           <>
