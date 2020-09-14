6cb2e35c8c095221010a184717a54f4e585c475c
Connie Chan <conniechan@connies-mbp-4.lan>
2020-09-14 02:23:20 -0400

    Updates

---
+++ precache-manifest.be8acd5db27fcff521898aa58325a603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fae7c752b8be84995822e5a7b3b856",
+    "url": "/acnh-villager-stylist/index.html"
+  },
+  {
+    "revision": "02aa4a392b1f10b927fe",
+    "url": "/acnh-villager-stylist/static/css/main.bbc9107f.chunk.css"
+  },
+  {
+    "revision": "6440fddf7ea136f415d7",
+    "url": "/acnh-villager-stylist/static/js/2.18f941c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/acnh-villager-stylist/static/js/2.18f941c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02aa4a392b1f10b927fe",
+    "url": "/acnh-villager-stylist/static/js/main.ac80bf71.chunk.js"
+  },
+  {
+    "revision": "1133a2bc2d94f530a97d",
+    "url": "/acnh-villager-stylist/static/js/runtime-main.212f0e7c.js"
+  },
+  {
+    "revision": "f7491e94f07df116402e2f308fa722eb",
+    "url": "/acnh-villager-stylist/static/media/ac-background-5.f7491e94.jpg"
+  }
+]);
