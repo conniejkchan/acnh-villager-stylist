f7089d815eb907f25b0752ea47f5639ee3bb8c7d
Connie Chan <conniechan@connies-mbp-4.lan>
2020-09-13 20:57:20 -0400

    villager search finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 import villagersData from './data/villagers.json';
-import ItemsData from "./data/items.json";
+import itemsData from "./data/items.json";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +22,7 @@ const useStyles = makeStyles((theme) => ({
   input: {
     marginLeft: theme.spacing(1),
     flex: 1,
+    textAlign: 'center'
   },
   iconButton: {
     padding: 10,
@@ -28,43 +31,125 @@ const useStyles = makeStyles((theme) => ({
     height: 28,
     margin: 4,
   },
+
 }));
 
 const App = ({}) => {
+  const [villagerName, setVillagerName] = useState('')
+  const [villagerNameColor, setVillagerNameColor] = useState('')
+  const [villagerPhoto, setVillagerPhoto] = useState('')
+  const [villagerColors, setVillagerColors] = useState([])
+  const [villagerStyles, setVillagerStyles] = useState([])
 
-  let villagerNameList = []
-  let villagerPhotoList = []
+  const[itemName, setItemName] = useState('')
+  const[itemPhoto, setItemPhoto] = useState('')
+  const[itemVariation,setItemVariation] = useState([])
 
-  for(let i=0; i<villagersData.length; i++){
-      villagerNameList[i] = villagersData[i].name
-  }
-
-  for(let i=0; i<villagersData.length; i++){
-    villagerPhotoList[i] = villagersData[i].photoImage
-  }
 
   const classes = useStyles();
+
+  // show villager pic and name
+  const getVillager = e => {
+    villagersData.map(villager => {
+      if(e.target.value.toLowerCase() === villager.name.toLowerCase()) {
+        setVillagerName(villager.name)
+        setVillagerPhoto(villager.photoImage)
+        setVillagerNameColor(villager.bubbleColor)
+        setVillagerColors(villager.colors)
+        setVillagerStyles(villager.styles)
+      }
+    })
+  }
+
+  const getItem = e => {
+    itemsData.map(item => {
+      if(e.target.value.toLowerCase() === item.name.toLowerCase()) {
+        setItemName(item.name)
+        //setItemPhoto(item)
+      }
+    })
+  }
+
   return (
     <>
-    <h1>Animal Crossing New Horizon Villager Stylists</h1>
-    <InputBase
-        className={classes.input}
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-    <IconButton type="submit" className={classes.iconButton} aria-label="search">
-      <SearchIcon />
-    </IconButton>
-    {villagersData.map(villager => {
-        return (
-          <>
-          <div>
-            <h2>{villager.name}</h2>
-            <img src={villager.photoImage} />
-          </div>
-          </>
-        );
-    })}
+    <h1 className="header">Animal Crossing New Horizon Villager Stylists</h1>
+    <Grid container spacing={3}>
+      <Grid item xs={6}>
+        <form noValidate autoComplete="off">
+          <TextField
+            className={classes.input}
+            id="outlined-search"
+            label="Villager Name"
+            type="search"
+            variant="outlined"
+            color="secondary"
+            onChange={getVillager}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+        </form>
+      </Grid>
+      <Grid item xs={6}>
+        <form noValidate autoComplete="off">
+          <TextField
+            className={classes.input}
+            id="outlined-search"
+            label="Item Name"
+            type="search"
+            variant="outlined"
+            onChange={getItem}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+        </form>
+      </Grid>
+      <Grid item xs={6}>
+        <div className="searchVillager">
+          <h2 style={{color: villagerNameColor}}>{villagerName}</h2>
+          <img src={villagerPhoto} />
+          <h3>likes 
+            {villagerColors.map(color => {
+              return (
+                <>
+                  <li>
+                    {color}
+                  </li>
+                </>
+              );
+            })}
+            {villagerStyles.map(style => {
+            return (
+              <>
+                <li>
+                  {style}
+                </li>
+              </>
+            );
+          })}
+          </h3>
+        </div>
+      </Grid>
+      <Grid item xs={6}>
+        <div className="searchItem">
+          <h2>{itemName}</h2>
+          <img src={villagerPhoto} />
+        </div>
+      </Grid>
+    </Grid>
   
     </>
   );
